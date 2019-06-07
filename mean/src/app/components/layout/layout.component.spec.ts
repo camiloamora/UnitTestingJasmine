50be59811699d58fc0eb4571b391810cc8c8d36e
Camilo Mora <kalufau@hotmail.com>
2019-06-07 17:49:06 -0500

    Validacion de las rutas en el componente de laoyut

--- mean/src/app/components/layout/layout.component.spec.ts
+++ mean/src/app/components/layout/layout.component.spec.ts
@@ -1,14 +1,38 @@
+import { MatBottomSheet } from '@angular/material';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('LayoutComponent', () => {
+class MatBottomSheetStub {
+  open() {}
+}
+
+fdescribe('LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
+      declarations: [ LayoutComponent ],
+      providers: [
+        {
+          provide: MatBottomSheet, useClass: MatBottomSheetStub
+        }
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([
+          {
+            path: '',
+            component: LayoutComponent
+        },
+        {
+          path: 'app/add',
+          component: LayoutComponent
+        }])
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -21,5 +45,17 @@ describe('LayoutComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should set the edit mode to false', () => {
+    const verifyEditMode = spyOn(component, 'verifyEditMode').and.callThrough();
+    fixture.ngZone.run(() => {
+      (<any>component).router.navigate(['/']);
+
+      fixture.whenStable().then(() => {
+        expect(component.editMode).toBeFalsy();
+        expect(verifyEditMode).toHaveBeenCalled();
+      });
+    });
   });
 });
